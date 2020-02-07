c71cd8d7566618553b06451dd79e78bbd867e6c3
James Holton <james.holton@yahoo.co.uk>
2020-02-07 15:27:47 +0000

    Changed something, not sure what

--- src/mazeSolver.js
+++ src/mazeSolver.js
@@ -21,7 +21,6 @@ MazeSolver.prototype.findJunctions = function(apiResponse){
 }
 
 MazeSolver.prototype.makeMove = async function(locationKey, command, repeat) {
-    // const apiResponse = mazeApiCall(locationKey, command, repeat).then((response) => { console.log('response: ', response);});
     const apiResponse = await mazeApiCall(locationKey, command, repeat)
     const apiSplit = apiResponse.split(',')[0]
     return apiSplit
@@ -56,15 +55,12 @@ MazeSolver.prototype.mainLine = async function(){
     let xFound = false;
     let locationID = await mazeApiCall();
     const startID = locationID.split(',')[0];
-    console.log('start', startID);
 
     let steps = this.lookForX(locationID);
     if (steps) xFound = true
 
-    console.log('initial steps', steps);
     
     const junctions = this.findJunctions(locationID);
-    console.log('junctions', junctions);
 
     for (let i = 0; i < junctions.length; i++) {
         let newLocation = await this.makeMove(startID, 'M', junctions[i][0])
@@ -72,23 +68,15 @@ MazeSolver.prototype.mainLine = async function(){
         this.addJunctionStatus(newLocation, junctions[i][1]);
     }
 
-    console.log('junction array', this.junctionStatusArray);
-
     for (var i = 0; i < this.junctionStatusArray.length && !xFound; i++) {
         let item = this.junctionStatusArray[i]
-        console.log('item', item);
         apiResponse = await mazeApiCall(item[0], item[1]);
         locationID = apiResponse.split(',')[0];
-        console.log('api response', apiResponse);
-        console.log('locationID:',locationID);
-
         steps = this.lookForX(apiResponse);
+        
         if (steps) xFound = true
-
-        console.log('steps to X', steps);
-
+        
         const junctions2 = this.findJunctions(apiResponse);
-        console.log('new juctions', junctions2);
         
         for (let i = 0; i < junctions2.length; i++) {
             let newLocation = await this.makeMove(locationID, 'M', junctions2[i][0])
@@ -98,6 +86,5 @@ MazeSolver.prototype.mainLine = async function(){
     if (xFound = true) {
         const xLocation = await this.makeMove(locationID, 'M', steps)
         console.log(xLocation)
-        console.log(this.junctionStatusArray)
     }
 }
